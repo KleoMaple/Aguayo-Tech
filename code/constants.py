db77196238f049d94a3184be4db142453808cd9d
Carlos Avila <carlos2003chas@gmail.com>
2023-11-20 11:29:49 -0600

    Se agregó el fichero de constantes y se modificaron las rutas de los assets

---
+++ code/constants.py
@@ -0,0 +1,13 @@
+
+"""
+    Constantes para utilizar en todo el sistema. En caso de que vean que tienen una 
+    variable que su valor no cambiará y que pueden separar de su lógica, pueden declararla aquí.
+"""
+
+# FRONTEND
+
+ASSETS_PATH = "assets/"
+
+
+# BACKEND
+
